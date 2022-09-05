2365edfeba34fc810f6074fe4bf06b6e7160c812
Lokesh598 <ashawat.lokesh@gmail.com>
2022-09-05 11:44:50 +0530

    C# syntax added

--- Overview/HelloWorld/Program.cs
+++ Overview/HelloWorld/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+// Console.WriteLine("Hello, World!");
+
+namespace HelloWorld
+{
+    class program {
+        static void Main(string[] args) {
+            Console.WriteLine("I am new to C#");
+        }
+    }
+}
