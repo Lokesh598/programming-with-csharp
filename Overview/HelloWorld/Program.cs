961be595a52b953248f5181edb92c770866ab1e4
Lokesh598 <ashawat.lokesh@gmail.com>
2022-09-05 11:54:45 +0530

    reading input from user

--- Overview/HelloWorld/Program.cs
+++ Overview/HelloWorld/Program.cs
@@ -7,6 +7,9 @@ namespace HelloWorld
     class program {
         static void Main(string[] args) {
             Console.WriteLine("I am new to C#");
+            Console.WriteLine("What is up");
+            var str = Console.ReadLine();
+            Console.WriteLine("My name is "+ str + " and I am doing great");
         }
     }
 }
